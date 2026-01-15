a2f243c1c7753279b8b038286680cf31c325092c
Bruno Silva <brssousa@gmail.com>
2026-01-14 21:39:52 -0300

    ADD falha no teste

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import net.bytebuddy.implementation.bytecode.Throw;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+        throw new Exception("Test Exception");
 	}
 
 }
